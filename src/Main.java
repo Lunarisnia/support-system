595d45b2f9654426cff01c39fb3f01dea41cabcb
Rio Arswendo Rachmad <r.arswendo.r@gmail.com>
2023-12-31 20:34:51 +0700

    feat: add interface for the ahp

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,76 @@
 import AHP.Goal;
 import AHP.Subject;
+import AHP.Analyzer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Subject foobar = new Subject("foobar");
-        foobar.setDeliveryTime(10.0f).setPrice(20.0f).setQuality(5.0f);
-
+        List<Subject> subjects = new ArrayList<>();
         Goal goal = new Goal();
-        goal.setService(0.25f).setPrice(0.25f).setQuality(0.25f).setDeliveryTime(0.25f);
 
-        // TODO: Set up Analytical Hierarchy Process
-        // 1. It takes a goal, and distributors
-        // 2. It takes each distributor's criteria and then multiply them by goal one another.
-        // 3. Since multiplying by goal will result to a percentage of the distributor's criteria add them all up
-        // 4. Set the Distributor's total score with the addition result
-        // 5. Add a method to return the Distributor with the highest score.
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            // TODO: add name and NPM
+            System.out.println("Kelompok 5");
+            System.out.println("Rio Arswendo Rachmad 202143500473");
+            System.out.println("Juliani Ester Assa 202143500473");
+            System.out.println("Welcome to Analytical Hierarchy Process Machine");
+            System.out.println("1. Add Distributor");
+            System.out.println("2. Add Goal");
+            System.out.println("3. Decide");
+            System.out.print("What do you want to do: ");
+            try {
+                int choice = Integer.parseInt(scanner.nextLine());
+                switch (choice) {
+                    case 1:
+                        System.out.print("Enter Distributor Name: ");
+                        String distributorName = scanner.nextLine();
+                        System.out.print("Enter Price Rating: ");
+                        Float priceRating = Float.parseFloat(scanner.nextLine());
+                        System.out.print("Enter Quality Rating: ");
+                        Float qualityRating = Float.parseFloat(scanner.nextLine());
+                        System.out.print("Enter Service Rating: ");
+                        Float serviceRating = Float.parseFloat(scanner.nextLine());
+                        System.out.print("Enter Delivery Time Rating: ");
+                        Float deliveryTimeRating = Float.parseFloat(scanner.nextLine());
 
-        System.out.println("Hello");
+                        Subject newSubject = new Subject(distributorName);
+                        newSubject.setPrice(priceRating).setQuality(qualityRating).setService(serviceRating)
+                                .setDeliveryTime(deliveryTimeRating);
+                        subjects.add(newSubject);
+                        break;
+                    case 2:
+                        System.out.print("Enter Goal Price Weight Percentage in decimal: ");
+                        goal.setPrice(Float.parseFloat(scanner.nextLine()));
+                        System.out.print("Enter Goal Quality Weight Percentage in decimal: ");
+                        goal.setQuality(Float.parseFloat(scanner.nextLine()));
+                        System.out.print("Enter Goal Service Weight Percentage in decimal: ");
+                        goal.setService(Float.parseFloat(scanner.nextLine()));
+                        System.out.print("Enter Goal Delivery Time Weight Percentage in decimal: ");
+                        goal.setDeliveryTime(Float.parseFloat(scanner.nextLine()));
+                        break;
+                    case 3:
+                        if (subjects.isEmpty()) {
+                            System.out.println("Please add subjects before deciding");
+                            break;
+                        }
+                        Analyzer analyzer = new Analyzer(subjects, goal);
+                        analyzer.ahp();
+
+                        Subject bestDistributor = analyzer.bestChoice();
+                        System.out.println("Best Distributor is: " + bestDistributor.getSubjectName());
+                        break;
+                    default:
+                        System.out.println("Invalid choice");
+                        break;
+                }
+            } catch (NumberFormatException err) {
+                System.out.println("Invalid Choice");
+            }
+            System.out.println();
+        }
     }
 }
